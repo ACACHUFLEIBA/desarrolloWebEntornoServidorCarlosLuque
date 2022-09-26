847bd8361e07f89acd91aedcb874316847239b5c
Carlos Luque <99803682+ACACHUFLEIBA@users.noreply.github.com>
2022-09-26 10:19:19 +0200

    Add files via upload

---
+++ eje4B.php
@@ -0,0 +1,22 @@
+<HTML>
+<HEAD><TITLE> EJ4B – Tabla Multiplicar</TITLE></HEAD>
+<BODY>
+<?php
+
+$numero="3";
+$resultado="";
+
+for($i = 1;$i <=10;$i++){
+    $resultado = $numero*$i;
+    echo"<br/>";
+
+    echo"$numero*$i=$resultado";
+}
+
+
+
+
+?>
+</BODY>
+</HTML>
+
