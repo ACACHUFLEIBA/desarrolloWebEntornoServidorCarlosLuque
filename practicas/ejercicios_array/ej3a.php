51fa34b70cf421d2aa4d8581a86aee4aec094c23
Carlos Luque <99803682+ACACHUFLEIBA@users.noreply.github.com>
2022-09-28 18:36:49 +0200

    Add files via upload

---
+++ practicas/ejercicios_array/ej3a.php
@@ -0,0 +1,29 @@
+<HTML>
+<HEAD><TITLE> : definir un array y almacenar los 20 primeros números binarios. Mostrar en la salida una tabla como la de la figura
+ </TITLE></HEAD>
+<BODY>
+<?php
+$binarios=array();
+$octal=array();
+$contadorBinarios= 0;
+echo "<table border ='1'>";
+echo "<th>Indice</th><th>binario</th><th>octal</th>";
+    echo "<tr>";
+    //calculamos el binario
+for($i= 0;$i < 20;$i++ ){
+    $binarios[] = decbin($i);
+  
+}
+    //calculamos el octal e imprimimos
+    for($i= 0;$i < 20;$i++ ){
+        $octal[] = decoct($i);
+        echo("<td>".$i."</td>");
+        echo("<td>".$binarios[$i]."</td>");
+        echo("<td>".$octal[$i]."</td>");
+    echo "</tr>";
+    }
+echo"</table>";
+
+?>
+</BODY>
+</HTML>
